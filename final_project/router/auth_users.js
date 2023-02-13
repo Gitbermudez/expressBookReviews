cb0da1b69f249d9e64cbe1a103f4bc57c842bbf9
Douglas B <103091004+Gitbermudez@users.noreply.github.com>
2023-02-13 00:16:08 -0600

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,30 +1,89 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [
+  {
+    username: "user1",
+    password: "password1",
+  },
+  {
+    username: "user2",
+    password: "password2",
+  },
+  {
+    username: "user3",
+    password: "password3",
+  },
+];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+  //returns boolean
+  //write code to check if username is already present in records.
+  return users.some((user) => user.username === username);
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  //returns boolean
+  //write code to check if username and password are present in records.
+  return users.some(
+    (user) => user.username === username && user.password === password
+  );
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
+regd_users.post("/login", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+  if (!authenticatedUser(username, password)) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+  const token = jwt.sign(
+    {
+      username,
+      password,
+    },
+    "secret",
+    { expiresIn: 60 * 60 }
+  );
+  req.session.username = username;
+  return res.status(200).json({ token });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params;
+  const { review } = req.body;
+  const { username } = req.session;
+  if (!review) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+  const book = books[isbn];
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  book.reviews.push({ username, review });
+  return res.status(200).json({ message: "Review added", book });
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  //Write your code here
+  const { isbn } = req.params;
+  const { username } = req.session;
+  const book = books[isbn];
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  book.reviews = book.reviews.filter((review) => review.username !== username);
+  return res.status(200).json({ message: "Review deleted" });
 });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
+module.exports.authenticatedUser = authenticatedUser;
